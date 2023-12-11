bb0cc3abd357054c5b92eb90882c040309fb5e09
lys306-gpu <dispink@icloud.com>
2023-12-11 10:58:23 +0000

    Test standardizing data
    Different learning rates are test using the standardized data
    (test_std.py). A NAN shows up so another test is carried out
    (test_nan.py). test_nan.py is modified from test_std.py.
    
    Outputs:
    results/loss_0.001/0.0001/1e-05/1e-06/1e-07_20231211.png
    results/test_std/lr_20231211.csv
    models/mae_vit_base_patch16_0.001/0.0001/1e-05_20231211.pth
    
    	new file:   pilot_07.ipynb
    	new file:   test_nan.py
    	new file:   test_std.py
    	modified:   util/datasets.py

---
+++ test_std.py
@@ -0,0 +1,124 @@
+"""
+This is a test file for training the model with different learning rate
+after standarizing the data.
+Also, I add torch.cuda.amp.autocast() to train_one_epoch() because now
+the spectrum data can be either float 32 or 64.
+"""
+
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+from util.datasets import CustomImageDataset
+from models_mae import mae_vit_base_patch16
+import matplotlib.pyplot as plt
+import datetime
+import time
+
+
+def get_date():
+    return datetime.date.today().strftime("%Y%m%d")
+
+def get_dataloader(batch_size: int):
+    dataset = CustomImageDataset('data/info_20231121.csv', 'data/spe', std=True)
+    data_train, data_val = random_split(dataset, [0.8, 0.2], generator=torch.manual_seed(24))
+    dataloader = {
+        'train':DataLoader(data_train, batch_size=batch_size, shuffle=True),
+        'val':DataLoader(data_val, batch_size=batch_size, shuffle=True)
+        }
+    return dataloader
+
+def train_one_epoch(model: nn.Module, dataloader: DataLoader, lr: float, device='cuda'):
+    """
+    lr: learning rate
+    """
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, betas=(0.9, 0.95))
+    model = model.to(device)
+
+    model.train()  # turn on train mode
+    total_loss = 0.
+
+    # remove step_loss_list
+    for samples in dataloader:
+        samples = samples.to(device, non_blocking=True, dtype=torch.float)
+        with torch.cuda.amp.autocast():
+            loss, _, _ = model(samples)
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+def evaluate(model: nn.Module, dataloader: DataLoader, device='cuda'):
+    total_loss = 0.
+    model.eval()  # turn on evaluation mode
+
+    with torch.no_grad():
+        for samples in dataloader:
+            samples = samples.to(device, non_blocking=True, dtype=torch.float)
+            loss, _, _ = model(samples)
+            total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+def visualize(train_loss_list, val_loss_list, lr, out_dir: str):
+    plt.figure()
+    plt.plot(train_loss_list, label='train loss')
+    plt.plot(val_loss_list, label='val loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.savefig(f'{out_dir}/loss_{lr}_{get_date()}.png')
+
+def trainer(model: nn.Module, dataloader: DataLoader, lr: float, epochs: int):
+    """
+    Train the model for epochs. Export the training and validation loss in figure.
+    Output the model's minimum validation loss.
+    """
+    train_loss_list = []
+    val_loss_list = []
+
+    for epoch in range(1, epochs+1):
+        epoch_start_time = time.time()
+
+        epoch_loss = train_one_epoch(model=model, dataloader=dataloader['train'], lr=lr)
+        train_loss_list.append(epoch_loss)
+
+        val_loss = evaluate(model, dataloader['val'])
+        val_loss_list.append(val_loss)
+
+        elapsed = time.time() - epoch_start_time
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+            f'valid loss {val_loss:.3f} ')
+        print('-' * 89)
+    
+    visualize(train_loss_list, val_loss_list, lr, 'results')
+    return min(val_loss_list)
+
+def main():
+    dataloader = get_dataloader(batch_size=64)
+    epochs = 100
+    best_val_loss = 1e7
+    val_loss_list = []
+    lr_list = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
+
+    for lr in lr_list:
+        # reset model
+        model = mae_vit_base_patch16()
+        val_loss = trainer(model, dataloader, lr, epochs)
+        val_loss_list.append(val_loss)
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), f'models/mae_vit_base_patch16_{lr}_{get_date()}.pth')
+    return lr_list, val_loss_list
+
+if __name__ == '__main__':
+    import pandas as pd
+    lr_list, val_loss_list = main()
+    df = pd.DataFrame({'lr':lr_list, 'min_val_loss':val_loss_list})
+    df.to_csv(f'results/test_lr_{get_date()}.csv', index=False)
+ 
--- util/datasets.py
+++ util/datasets.py
@@ -4,17 +4,39 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, input_dir):
+    def __init__(self, annotations_file, input_dir, std=True):
         """
         input_dir: directory with spe files
         """
         self.spe_info = pd.read_csv(annotations_file)
         self.input_dir = input_dir
+        self.std = std
         
     def __len__(self):
         return len(self.spe_info)
 
     def __getitem__(self, idx):
         input_path = os.path.join(self.input_dir, self.spe_info.iloc[idx, 0])
-        spe = np.loadtxt(input_path, delimiter=',', dtype=int)  
+        spe = np.loadtxt(input_path, delimiter=',', dtype=int)
+        if self.std:
+            spe = self.standardize(spe)
         return spe
+    
+    def standardize(self, spe):
+        """
+        spe: numpy array of shape (n_channels)
+        Standardize the spectrum to have zero mean and unit variance.
+        """
+        mean = np.mean(spe, axis=0)
+        std = np.std(spe, axis=0)
+        spe = (spe - mean) / std
+        return spe
+    
+    
+if __name__ == "__main__":
+    dataset = CustomImageDataset(annotations_file='data/info_20231121.csv', input_dir='data/spe', std=True)
+    spe = dataset[0]
+    print(spe.shape)
+    print(spe.dtype)
+    print(spe)
+    print(spe.max())
