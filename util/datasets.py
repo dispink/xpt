fc94d3f813c8e48bf7598050ea56b3c34a6c7e61
lys306-gpu <dispink@icloud.com>
2023-12-12 08:47:39 +0000

    Test log transform and compare with previous model
    The results are noted in pilot_08.ipynb.
    datasets.py was modified to include the log transform.
    test_log.py was created to train and test the model.
    
    Outputs:
    results/loss_ln_0.001/0.0001_20231212.png
    results/loss_ln_1e-05/6/7_20231212.png
    models/mae_vit_base_patch16_ln_0.001/0.0001_20231212.pth
    
    	new file:   pilot_08.ipynb
    	new file:   test_log.py
    	modified:   util/datasets.py

--- util/datasets.py
+++ util/datasets.py
@@ -4,13 +4,19 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, input_dir, std=True):
+    
+    def __init__(self, annotations_file: str, input_dir: str, transform: object=None):
         """
-        input_dir: directory with spe files
+        Initialize a Dataset object.
+
+        Args:
+            annotations_file (str): Path to the annotations file.
+            input_dir (str): Directory containing spe files.
+            transform (function, optional): Optional data transformation object. Defaults to None.
         """
         self.spe_info = pd.read_csv(annotations_file)
         self.input_dir = input_dir
-        self.std = std
+        self.transform = transform
         
     def __len__(self):
         return len(self.spe_info)
@@ -18,25 +24,34 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         input_path = os.path.join(self.input_dir, self.spe_info.iloc[idx, 0])
         spe = np.loadtxt(input_path, delimiter=',', dtype=int)
-        if self.std:
-            spe = self.standardize(spe)
+        if self.transform:
+            spe = self.transform(spe)
         return spe
     
-    def standardize(self, spe):
-        """
-        spe: numpy array of shape (n_channels)
-        Standardize the spectrum to have zero mean and unit variance.
-        """
-        mean = np.mean(spe, axis=0)
-        std = np.std(spe, axis=0)
-        spe = (spe - mean) / std
-        return spe
-    
+def standardize(spe):
+    """
+    spe: numpy array of shape (n_channels)
+    Standardize the spectrum to have zero mean and unit variance.
+    """
+    mean = np.mean(spe, axis=0)
+    std = np.std(spe, axis=0)
+    spe = (spe - mean) / std
+    return spe
+
+def log_transform(spe):
+    """
+    spe: numpy array of shape (n_channels)
+    Apply log transform to the spectrum.
+    Add 1 to avoid log(0).
+    """
+    spe = np.log(spe+1)
+    return spe 
     
 if __name__ == "__main__":
-    dataset = CustomImageDataset(annotations_file='data/info_20231121.csv', input_dir='data/spe', std=True)
-    spe = dataset[0]
-    print(spe.shape)
-    print(spe.dtype)
-    print(spe)
-    print(spe.max())
+    for transfom in [None, standardize, log_transform]:
+        dataset = CustomImageDataset(annotations_file='data/info_20231121.csv', input_dir='data/spe', transform=transfom)
+        spe = dataset[60]
+        print(spe.shape)
+        print(spe.dtype)
+        print(spe)
+        print(spe.max())
