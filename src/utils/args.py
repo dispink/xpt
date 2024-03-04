7fdc500d8965dff11d3d019da6b587a5c3e44b85
paoyw <vincent01pao@gmail.com>
2024-03-04 15:44:41 +0800

    Add src.utils.get_tune_args

--- src/utils/args.py
+++ src/utils/args.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser, Namespace
 
 
-def get_train_parser() -> Namespace:
+def get_train_args() -> Namespace:
     parser = ArgumentParser()
     # General parameters
     parser.add_argument('--data_path')
@@ -25,8 +25,45 @@ def get_train_parser() -> Namespace:
     # Optimizer parameters
     parser.add_argument('--weight_decay', default=0, type=float)
     parser.add_argument('--blr', type=float)
-    parser.add_argument('--min_lr', type=float)
+    parser.add_argument('--min_lr', default=0, type=float)
     parser.add_argument('--warmup_epochs', type=int)
     parser.add_argument('--accum_iter', default=1, type=int)
+
+    return parser.parse_args()
+
+
+def get_tune_args() -> Namespace:
+    parser = ArgumentParser()
+    # General parameters
+    parser.add_argument('--data_path')
+    parser.add_argument('--output_dir')
+    parser.add_argument('--log_dir')
+
+    # Device parameters
+    parser.add_argument('--device', default='cpu')
+    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--pin_memory', action='store_true')
+
+    # Model parameters
+    parser.add_argument('--model', default='base',
+                        choices=['base', 'large', 'huge'])
+    
+    # Fine-tune parameters
+    parser.add_argument('--finetune', required=True)
+    parser.add_argument('--global_pool', action='store_true')
+    parser.add_argument('--cls_token', action='store_true')
+    
+    # Hyper-parameters
+    parser.add_argument('--batch_size', ytpe=int)
+    parser.add_argument('--epochs', type=int)
+
+    # Optimizer parameters
+    parser.add_argument('--clip_grad', type=float)
+    parser.add_argument('weight_decay', type=float)
+    parser.add_argument('--blr', type=float)
+    parser.add_argument('--min_lr', defulat=0, type=float)
+    parser.add_argument('--layer_decay', type=float)
+    parser.add_argument('--warmup_epochs', type=int)
+    parser.add_argument('--accum_iter', type=int)
 
     return parser.parse_args()
