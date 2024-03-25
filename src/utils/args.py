fa9218f83daf6a1573aee49d82fdc48b88a3362a
paoyw <b09902016@csie.ntu.edu.tw>
2024-03-25 09:27:06 +0800

    Add optimizer and lr_scheduler

--- src/utils/args.py
+++ src/utils/args.py
@@ -4,31 +4,36 @@ from argparse import ArgumentParser, Namespace
 def get_train_args() -> Namespace:
     parser = ArgumentParser()
     # General parameters
-    parser.add_argument('--data_path')
-    parser.add_argument('-output_dir', required=True)
-    parser.add_argument('--resume', action='store_true')
+    parser.add_argument("--data_path")
+    parser.add_argument("-output_dir", required=True)
+    parser.add_argument("--resume", action="store_true")
 
     # Device parameters
-    parser.add_argument('--device', default='cpu')
-    parser.add_argument('--num_workers', default=8, type=int)
-    parser.add_argument('--pin_memory', action='store_true')
+    parser.add_argument("--device", default="cpu")
+    parser.add_argument("--num_workers", default=8, type=int)
+    parser.add_argument("--pin_memory", action="store_true")
 
     # Model parameters
-    parser.add_argument('--model', default='base',
-                        choices=['base', 'large', 'huge'])
-    parser.add_argument('--mask_ratio', default=0.8, type=float)
+    parser.add_argument("--model", default="base", choices=["base", "large", "huge"])
+    parser.add_argument("--mask_ratio", default=0.8, type=float)
 
     # Hyper-parameters
-    parser.add_argument('--batch_size', type=int)
-    parser.add_argument('--epochs', type=int)
+    parser.add_argument("--batch_size", type=int)
+    parser.add_argument("--epochs", type=int)
 
     # Optimizer parameters
-    parser.add_argument('--optim', default='AdamW')
-    parser.add_argument('--weight_decay', default=0, type=float)
-    parser.add_argument('--blr', type=float)
-    parser.add_argument('--min_lr', default=0, type=float)
-    parser.add_argument('--warmup_epochs', type=int)
-    parser.add_argument('--accum_iter', default=1, type=int)
+    parser.add_argument("--optim", default="AdamW")
+    parser.add_argument("--weight_decay", default=0, type=float)
+    parser.add_argument("--blr", type=float)
+    parser.add_argument("--min_lr", default=0, type=float)
+    parser.add_argument("--warmup_epochs", type=int)
+    parser.add_argument("--accum_iter", default=1, type=int)
+
+    # Learning rate scheduler parameters
+    parser.add_argument("--lr_scheduler", default="warmup-consine-annealing")
+    parser.add_argument("--warmup_epochs", default=10, type=int)
+    parser.add_argument("--warmup_start_factor", default=0.5, type=float)
+    parser.add_argument("--annealing_epochs", default=70)
 
     return parser.parse_args()
 
@@ -36,39 +41,45 @@ def get_train_args() -> Namespace:
 def get_tune_args() -> Namespace:
     parser = ArgumentParser()
     # General parameters
-    parser.add_argument('--data_path')
-    parser.add_argument('--output_dir')
-    parser.add_argument('--log_dir')
+    parser.add_argument("--data_path")
+    parser.add_argument("--output_dir")
+    parser.add_argument("--log_dir")
 
     # Device parameters
-    parser.add_argument('--device', default='cpu')
-    parser.add_argument('--num_workers', default=8, type=int)
-    parser.add_argument('--pin_memory', action='store_true')
+    parser.add_argument("--device", default="cpu")
+    parser.add_argument("--num_workers", default=8, type=int)
+    parser.add_argument("--pin_memory", action="store_true")
 
     # Model parameters
-    parser.add_argument('--model', default='base',
-                        choices=['base', 'large', 'huge'])
+    parser.add_argument("--model", default="base", choices=["base", "large", "huge"])
 
     # Fine-tune parameters
-    parser.add_argument('--finetune', required=True)
-    parser.add_argument('--predict_layer', default='cls_token',
-                        choices=['cls_token', 'global_pool'])
+    parser.add_argument("--finetune", required=True)
+    parser.add_argument(
+        "--predict_layer", default="cls_token", choices=["cls_token", "global_pool"]
+    )
 
     # Hyper-parameters
-    parser.add_argument('--batch_size', ytpe=int)
-    parser.add_argument('--epochs', type=int)
+    parser.add_argument("--batch_size", ytpe=int)
+    parser.add_argument("--epochs", type=int)
 
     # Optimizer parameters
-    parser.add_argument('--optim', default='AdamW')
-    parser.add_argument('--lr_scheduler', default='ConstantLR')
-    parser.add_argument('--clip_grad', type=float)
-    parser.add_argument('--weight_decay', type=float)
-    parser.add_argument('--blr', type=float)
-    parser.add_argument('--min_lr', defulat=0, type=float)
-    parser.add_argument('--betas', defulat=(0.9, 0.95), type=float, nargs=2)
-    parser.add_argument('--layer_decay', type=float)
-    parser.add_argument('--warmup_epochs', type=int)
-    parser.add_argument('--accum_iter', type=int)
+    parser.add_argument("--optim", default="AdamW")
+    parser.add_argument("--lr_scheduler", default="ConstantLR")
+    parser.add_argument("--clip_grad", type=float)
+    parser.add_argument("--weight_decay", type=float)
+    parser.add_argument("--blr", type=float)
+    parser.add_argument("--min_lr", defulat=0, type=float)
+    parser.add_argument("--betas", defulat=(0.9, 0.95), type=float, nargs=2)
+    parser.add_argument("--layer_decay", type=float)
+    parser.add_argument("--warmup_epochs", type=int)
+    parser.add_argument("--accum_iter", type=int)
+
+    # Learning rate scheduler parameters
+    parser.add_argument("--lr_scheduler", default="warmup-consine-annealing")
+    parser.add_argument("--warmup_epochs", default=10, type=int)
+    parser.add_argument("--warmup_start_factor", default=0.5, type=float)
+    parser.add_argument("--annealing_epochs", default=70)
 
     return parser.parse_args()
 
@@ -76,18 +87,18 @@ def get_tune_args() -> Namespace:
 def get_eval_args() -> Namespace:
     parser = ArgumentParser()
     # General parameters
-    parser.add_argument('--pretrain')
-    parser.add_argument('--downstream')
-    parser.add_argument('--data_path')
-    parser.add_argument('--checkpoint')
-    parser.add_argument('--output_dir')
-    parser.add_argument('--log_dir')
+    parser.add_argument("--pretrain")
+    parser.add_argument("--downstream")
+    parser.add_argument("--data_path")
+    parser.add_argument("--checkpoint")
+    parser.add_argument("--output_dir")
+    parser.add_argument("--log_dir")
 
     # Device parameters
-    parser.add_argument('--device', default='cpu')
-    parser.add_argument('--num_workers', default=8, type=int)
-    parser.add_argument('--pin_memory', action='store_true')
+    parser.add_argument("--device", default="cpu")
+    parser.add_argument("--num_workers", default=8, type=int)
+    parser.add_argument("--pin_memory", action="store_true")
 
     # Inference parameters
-    parser.add_argument('--batch_size', ytpe=int)
+    parser.add_argument("--batch_size", ytpe=int)
     return parser.add_argument()
--- src/utils/optim.py
+++ src/utils/optim.py
@@ -1,16 +1,32 @@
-from torch import optim
+from torch import optim, lr_scheduler
 
 
 def get_optimizer(params, args):
-    if args.optim == 'AdamW':
+    if args.optim == "Adam":
+        optimizer = optim.Adam(params, args.blr)
+    elif args.optim == "AdamW":
         optimizer = optim.AdamW(params, args.blr, betas=tuple(args.betas))
     else:
-        raise NotImplementedError(f'{args.optim} is not implemented.')
+        raise NotImplementedError(f"{args.optim} is not implemented.")
     return optimizer
 
 
 def get_lr_scheduler(optimizer, args):
-    return
+    if args.lr_scheduler == "warmup-cosine-annealing":
+        scheduler0 = lr_scheduler.LinearLR(
+            optimizer, start_factor=args.warmup_start_factor, end_factor=1.0
+        )
+        scheduler1 = lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args.annealing_epochs
+        )
+        scheduler = lr_scheduler.SequentialLR(
+            optimizer,
+            schedulers=[scheduler0, scheduler1],
+            milestones=[args.warmup_epochs, args.annealing_epochs],
+        )
+    else:
+        raise NotImplementedError(f"{args.lr_scheduler} is not implemented.")
+    return scheduler
 
 
 def get_optimizer_lr_scheduler(params, args):
