3736e1ad507eb8e53400156eb228eaefe393f507
lys306-gpu <dispink@icloud.com>
2024-04-03 04:13:46 +0000

    Migrate plot_slides.py

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 
 def demo_patch(root: str = os.getcwd()):
@@ -51,6 +52,90 @@ def demo_patch(root: str = os.getcwd()):
     fig.savefig(f"{root}/results/demo_patch_2.png", transparent=True)
 
 
+def unpatchify_PredandMask(mask, pred, model):
+    pred_un = model.unpatchify(pred)
+    pred_un_arr = pred_un.squeeze(0).cpu().numpy()
+
+    mask_arr = mask.squeeze(0).cpu().numpy()
+    mask_un_arr = np.array([])
+    for i in mask_arr:
+        mask_un_arr = np.concatenate((mask_un_arr, np.repeat(i, 16)))
+    mask_un_arr = mask_un_arr.astype(int)
+
+    return pred_un_arr, mask_un_arr
+
+
+def demo_reconstruction(root: str = os.getcwd()):
+    import torch
+    from src.datas import datasets, transforms, dataloader
+    from src.models import mae_vit
+
+    # load dataset
+
+    dataset = datasets.PretrainDataset(
+        annotations_file=f"{root}/data/pretrain/train/info.csv",
+        input_dir=f"{root}/data/pretrain/train/spe",
+    )
+
+    _, data_val = dataloader.split(dataset)
+
+    print(data_val[22])
+
+    model = mae_vit.mae_vit_base_patch16().to("cuda")
+    model.load_state_dict(
+        torch.load("models/mae_vit_base_patch16_l-coslr_1e-05_20231229.pth")
+    )
+
+    model.eval()
+    with torch.no_grad():
+        spe_arr = transforms.standardize_numpy(data_val[22].numpy())
+        spe = (
+            torch.tensor(spe_arr)
+            .unsqueeze(0)
+            .to("cuda", non_blocking=True, dtype=torch.float)
+        )
+        _, pred, mask = model(spe)
+        pred_un_arr, mask_un_arr = unpatchify_PredandMask(mask, pred, model)
+
+    # create figures with transparent background
+    channel = np.arange(1, len(spe_arr) + 1)
+    ylim = (-1, 11.8)
+
+    # plot the masked spectrum
+    fig = plt.figure(figsize=(7, 5))
+    plt.plot(channel, spe_arr, alpha=0.8, label="target", c="C0")
+    plt.ylim(ylim)
+    plt.vlines(
+        channel,
+        ymin=-0.5,
+        ymax=mask_un_arr * (spe_arr.max()),
+        color="white",
+        label="masked",
+    )
+    plt.xlabel("Channel")
+    plt.ylabel("Standardized intensity")
+    plt.tight_layout()
+    plt.savefig(f"{root}/results/spe_with_mask.png", transparent=True)
+
+    # plot the unmasked spectrum
+    fig = plt.figure(figsize=(7, 5))
+    plt.plot(channel, spe_arr, alpha=0.8, label="target", c="C0")
+    plt.ylim(ylim)
+    plt.xlabel("Channel")
+    plt.ylabel("Standardized intensity")
+    plt.tight_layout()
+    plt.savefig(f"{root}/results/spe_without_mask.png", transparent=True)
+
+    # plot the predicted spectrum
+    fig = plt.figure(figsize=(7, 5))
+    plt.plot(channel, pred_un_arr, alpha=0.8, label="target", c="C1")
+    plt.ylim(ylim)
+    plt.xlabel("Channel")
+    plt.ylabel("Standardized intensity")
+    plt.tight_layout()
+    plt.savefig(f"{root}/results/pred_without_mask.png", transparent=True)
+
+
 if __name__ == "__main__":
     # get root directory
-    demo_patch()
+    demo_reconstruction()
