18f764ab5fd4a3756644b16cdac03d7a1e5f45be
paoyw <vincent01pao@gmail.com>
2024-06-10 17:17:38 +0800

    Add restart time

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 
 def get_log_writer(args):
     w = SummaryWriter(args.output_dir)
-    w.add_hparams(vars(args), {})
+    hparam_dict = vars(args)
+    hparam_dict['start-time'] = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    w.add_hparams(hparam_dict, {})
     return w
