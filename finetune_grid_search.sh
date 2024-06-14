e7476acf4715871b0ee05b2c6d3166f5acfc6a18
paoyw <vincent01pao@gmail.com>
2024-06-14 12:17:04 +0800

    Add evaluation for finetune and shell script for finding params for finetune

---
+++ finetune_grid_search.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+target=CaCO3
+annotation_files=(
+    'info_train_10.csv'
+    'info_train_50.csv'
+    'info_train_100.csv'
+    'info_train_500.csv'
+    'info_train_1000.csv'
+    'info_train_1500.csv'
+    'info.csv'
+)
+
+epochs=(10 25 50 75 100)
+warm_ups=(1 3 5 8 10)
+lrs=(1e-4 1e-5 1e-6)
+
+
+for annotation_file in ${annotation_files[*]};
+do
+    for lr in ${lrs[*]};
+    do
+        for (( i=0; i<${#epochs[*]}; ++i));
+        do
+            epoch=${epochs[$i]}
+            warm_up=${warm_ups[$i]}
+            output_dir="results/finetune-$target-$annotation_file-epochs-$epoch-blr-$lr/"
+            echo "START $target train=$annotation_file, blr=$lr, epochs=$epoch, warm_up=$warm_up"
+            python eval_finetune.py --target $target --weight "$output_dir/model.ckpt"
+            # python finetune.py \
+            #     --annotation_file data/finetune/${target}%/train/$annotation_file \
+            #     --input_dir data/finetune/${target}%/train \
+            #     --output_dir $output_dir \
+            #     --verbose \
+            #     --device cuda \
+            #     --pretrained_weight pretrain_test_20240611_model.ckpt \
+            #     --batch_size 256 \
+            #     --epochs $epoch \
+            #     --warmup_epochs $warm_up \
+            #     --blr $lr \
+            #     --target_mean src/datas/xpt_${target}_target_mean.pth \
+            #     --target_std src/datas/xpt_${target}_target_std.pth
+        done
+    done
+done
