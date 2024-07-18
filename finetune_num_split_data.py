0022df3f76117b1e8bc0a4e9ae126e340b7f8304
lys306-gpu <dispink@icloud.com>
2024-07-18 04:00:40 +0000

    Remove 1500

--- finetune_num_split_data.py
+++ finetune_num_split_data.py
@@ -3,13 +3,13 @@ from sklearn.model_selection import train_test_split
 
 from src.datas.dataloader import split
 
-target = 'TOC'
+target = 'CaCO3'
 info_file = f'data/finetune/{target}%/train/info.csv'
 total_df = pd.read_csv(info_file)
 train, val = split(total_df)
 train_df = total_df.iloc[train.indices]
 
-for num in (10, 50, 100, 500, 1000, 1500):
+for num in (10, 50, 100, 500, 1000):
     sub_train_df, _ = train_test_split(train_df, train_size=num)
     sub_train_df.to_csv(
         f'data/finetune/{target}%/train/info_train_{num}.csv',
